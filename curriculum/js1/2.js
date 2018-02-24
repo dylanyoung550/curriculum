e96196eda307437087b820bd5f9265920c9cdb07
Dylan <dylan@garagescript.org>
2018-02-24 14:59:05 -0800

    added solution for js1, problem 2

--- curriculum/js1/2.js
+++ curriculum/js1/2.js
@@ -8,12 +8,13 @@
  */
 
 const solution = (a, b, c) => {
-  if(a > b && a > c) {
+  if (a > b && a > c) {
     return a;
-  } else if(b > a && b > c) {
+  } else if (b > a && b > c) {
     return b;
-  } else if(c > b && c > a) {
+  } else if (c > a && c > b) {
     return c;
+  }
 };
 
 module.exports = {
