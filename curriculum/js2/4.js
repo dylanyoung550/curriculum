2fd1d4060276c285ec08ecdc42596377a789a44c
dylanyoung550@gmail.com <dylanyoung550@gmail.com>
2018-05-06 11:08:42 -0700

    Js2/4 Solution

--- curriculum/js2/4.js
+++ curriculum/js2/4.js
@@ -4,8 +4,11 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (a, sum = 0, i = 0) => {
+  if(a.length===0) return '';
+  if(i===a.length) return sum;
+  sum += a[i];
+  return solution(a, sum, i + 1);
 };
 
 module.exports = {
